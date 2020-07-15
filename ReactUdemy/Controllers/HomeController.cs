d3ad4fbf8c40d426b7e76604b59d4f041a0e1692
yohsii <yohsii@hotmail.co.uk>
2020-07-15 11:45:02 +0100

    react udemy approval card and comment detail

---
+++ ReactUdemy/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReactUdemy.Models;
+
+namespace ReactUdemy.Controllers
+{
+    [ApiController]
+    [Route("api/[action]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //[Route("api/test")]
+        public IActionResult Test() {
+            return Json("testing123...");
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ReactUdemy/app/src/index.js
@@ -0,0 +1,32 @@
+﻿//import react and reactdom libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+//create react component
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45pm" text="Nice blog post!" avatarImg={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00am" text="Enjoyed it, thanks!" avatarImg={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00pm" text="Nice one!" avatarImg={faker.image.avatar()} />
+            </ApprovalCard>
+        </div>
+    );
+}
+
+//take react component and show it on screen
+ReactDOM.render(<App />, document.querySelector("#root"));
