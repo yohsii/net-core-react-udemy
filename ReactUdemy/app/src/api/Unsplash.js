b611e036ab0e13b4b918e43aa51740238f87c65c
yohsii <yohsii@hotmail.co.uk>
2020-07-20 11:58:58 +0100

    youtube app start

--- ReactUdemy/app/src/api/Unsplash.js
+++
@@ -1,6 +0,0 @@
-﻿import axios from 'axios';
-
-export default axios.create({
-    baseURL:'https://api.unsplash.com',
-    headers: { Authorization: 'Client-ID LThnNvyzXthKPvEpbE-9m631LZqO8XlMZMPfn6StBwk' }
-});
