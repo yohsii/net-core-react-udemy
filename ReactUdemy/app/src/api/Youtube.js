c00070c62afbe7ed2da56a407c2a6df691b4b692
yohsii <yohsii@hotmail.co.uk>
2020-07-31 11:55:24 +0100

    simple songs list and details app, an intro to redux

--- ReactUdemy/app/src/api/Youtube.js
+++
@@ -1,8 +0,0 @@
-﻿import axios from 'axios';
-
-const KEY = 'AIzaSyDcmjGeV_xgnK4wfWQHTT3DwS4e4tcvrNc';
-
-export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3'
-    , params: { part: 'snippet', maxResults: 5, type: 'video', key: KEY}
-});
--- ReactUdemy/app/src/components/App.js
+++ ReactUdemy/app/src/components/App.js
@@ -1,52 +1,10 @@
-﻿import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from '../api/Youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+﻿import React, { useState, useEffect } from 'react';
 
-const KEY = 'AIzaSyDcmjGeV_xgnK4wfWQHTT3DwS4e4tcvrNc';
+const App = (props) => {
 
-class App extends React.Component {
-    state = {videos:[],selectedVideo:null};
-
-    componentDidMount = () => {
-        this.onTermSubmit("buildings");
-    }
-
-    onTermSubmit = async (term) => {
-        const response = await youtube.get("/search", {
-            params: {
-                q: term
-            }
-        });
-        console.log("youtube res", response);
-        this.setState({ videos: response.data.items, selectedVideo:response.data.items[0] });
-    }
-
-    onVideoSelect = (video) => {
-        console.log("from video component", video);
-        this.setState({ selectedVideo:video });
-    }
-
-    render() {
-        return (
-
-            <div className="ui container">
-                <SearchBar onTermSubmit={this.onTermSubmit} />
-                <div className="ui grid">
-                    <div className="ui row">
-                        <div className="eleven wide column">
-                            <VideoDetail video={this.state.selectedVideo} />
-                        </div>
-                        <div className="five wide column">
-                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-        );
-    }
+    return (
+        <div>App</div>    
+    );
 }
 
 export default App;
